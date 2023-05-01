cf1b4f693ae44d8f108e5b3a4023396fbe9665ef
shohedul-islam <shohedulislam564@gmail.com>
2023-05-01 14:58:17 +0600

    first commit

---
+++ dom4.js
@@ -0,0 +1,17 @@
+const btn = document.querySelector('#btn');
+
+
+
+
+btn.addEventListener('click',(e)=>{ 
+   let  checkbox = document.querySelectorAll("input[name='color']:checked");
+   
+   let values = [];
+    
+checkbox.forEach((checkbox)=>{
+    values.push(checkbox.value)
+});
+alert(values);
+
+})
+
